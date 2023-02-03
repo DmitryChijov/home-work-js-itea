b40ab9b1590638ff3f82959062473e027c409a1d
DmitryChijov <chijow.dmitr@gmail.com>
2023-02-03 12:58:19 +0200

    переместил папку

---
+++ script/hw-6.js
@@ -0,0 +1,134 @@
+// 6. Вывести на экран таблицу умножения от 0 до 9.
+
+let a = 0;
+let b = 1;
+let c = 0;
+
+// таблица умножение на 1
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
+document.write('<br/>');
+
+
+// таблица умножение на 2
+
+a = 0;
+b = 2;
+
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
+document.write('<br/>');
+
+
+// таблица умножение на 3
+
+a = 0;
+b = 3;
+
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
+document.write('<br/>');
+
+
+// таблица умножение на 4
+
+a = 0;
+b = 4;
+
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
+document.write('<br/>');
+
+
+// таблица умножение на 5
+
+a = 0;
+b = 5;
+
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
+document.write('<br/>');
+
+
+// таблица умножение на 6
+
+a = 0;
+b = 6;
+
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
+document.write('<br/>');
+
+
+// таблица умножение на 7
+
+a = 0;
+b = 7;
+
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
+document.write('<br/>');
+
+
+// таблица умножение на 8
+
+a = 0;
+b = 8;
+
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
+document.write('<br/>');
+
+
+// таблица умножение на 9
+
+a = 0;
+b = 9;
+     
+for (i =  0; i <= 10; i++) {
+    c = a * b;
+    document.write(`${a} * ${b} = ${c}`);
+    document.write('<br/>');
+    a += 1;
+};
+
